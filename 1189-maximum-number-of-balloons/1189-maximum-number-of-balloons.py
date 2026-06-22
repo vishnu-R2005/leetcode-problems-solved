8cd31509064c482fcb8d223582fefb604bc26703
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-06-22 18:16:58 +0530

    Time: 3 ms (61.87%), Space: 19.3 MB (38.08%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        count = Counter(text)
+
+        return min(
+            count['b'],
+            count['a'],
+            count['l'] // 2,
+            count['o'] // 2,
+            count['n']
+        )
