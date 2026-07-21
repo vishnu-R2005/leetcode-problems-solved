9b2f280e77ebe2f6f5f182f3d613b376ce37540a
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-07-21 10:56:22 +0530

    Time: 110 ms (73.57%), Space: 78.7 MB (25.8%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.head = Node()
+        self.tail = Node()
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+
+        prev.next = nxt
+        nxt.prev = prev
+
+    def insert(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+
+        self.remove(node)
+        self.insert(node)
+
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        node = Node(key, value)
+
+        self.insert(node)
+        self.cache[key] = node
+
+        if len(self.cache) > self.capacity:
+
+            lru = self.tail.prev
+
+            self.remove(lru)
+
+            del self.cache[lru.key]
